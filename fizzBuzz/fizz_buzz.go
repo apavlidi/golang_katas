4744b7067943cd5cf9e40bb74b107d5b08af62c1
Alexandros <alexandrospavlidis@monzo.com>
2025-05-04 12:08:57 +0100

    add fizz buzz code kata

--- fizzBuzz/fizz_buzz.go
+++ fizzBuzz/fizz_buzz.go
@@ -1,1 +1,17 @@
 package fizzBuzz
+
+import "strconv"
+
+func FizzBuzz(n int) string {
+	if n%15 == 0 {
+		return "FizzBuzz"
+	}
+	if n%3 == 0 {
+		return "Fizz"
+	}
+	if n%5 == 0 {
+		return "Buzz"
+	}
+
+	return strconv.Itoa(n)
+}
--- fizzBuzz/fizz_buzz_test.go
+++ fizzBuzz/fizz_buzz_test.go
@@ -1,1 +1,27 @@
 package fizzBuzz
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func TestFizzBuzz(t *testing.T) {
+	tests := []struct {
+		input    int
+		expected string
+	}{
+		{1, "1"},
+		{2, "2"},
+		{3, "Fizz"},
+		{4, "4"},
+		{5, "Buzz"},
+		{15, "FizzBuzz"},
+	}
+
+	for _, test := range tests {
+		t.Run(strconv.Itoa(test.input), func(t *testing.T) {
+			assert.Equal(t, test.expected, FizzBuzz(test.input))
+		})
+	}
+}
