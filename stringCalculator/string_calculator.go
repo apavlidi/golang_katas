923fd734cd7b561ceeaf4b0bd6ff1b729084b301
Alexandros <alexandrospavlidis@monzo.com>
2025-05-05 15:39:31 +0100

    add basic function

--- stringCalculator/string_calculator.go
+++ stringCalculator/string_calculator.go
@@ -1,1 +1,24 @@
 package stringCalculator
+
+import (
+	"strconv"
+	"strings"
+)
+
+func StringCalculator(s string) (int, error) {
+	if len(s) == 0 {
+		return 0, nil
+	}
+
+	split := strings.Split(s, ",")
+	sum := 0
+	for _, val := range split {
+		atoi, err := strconv.Atoi(val)
+		if err != nil {
+			return -1, err
+		}
+		sum += atoi
+	}
+
+	return sum, nil
+}
---
+++ stringCalculator/string_calculator_test.go
@@ -0,0 +1,31 @@
+package stringCalculator
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestStringCalculator(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected int
+	}{
+		{"Add empty string", "", 0},
+		{"Add 1,2", "1,2", 3},
+		{"Add 4", "4", 4},
+	}
+
+	for _, tc := range tests {
+		actual, err := StringCalculator(tc.input)
+		require.NoError(t, err)
+		assert.Equal(t, tc.expected, actual)
+	}
+}
+
+func TestStringCalculatorInvalidChars(t *testing.T) {
+	actual, err := StringCalculator("1,2,s,5")
+	require.Error(t, err)
+	assert.Equal(t, -1, actual)
+}
