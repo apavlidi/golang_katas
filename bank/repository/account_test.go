ffa014b607faccfa2e235de85a2ab2716af1c916
apavlidi <aleksispavlidis@gmail.com>
2025-05-17 23:01:35 +0300

    add tests in repo

--- bank/repository/account_test.go
+++ bank/repository/account_test.go
@@ -2,23 +2,42 @@ package repository
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"testing"
+	"time"
 )
 
 func TestPersonalAccountRepository_Deposit(t *testing.T) {
-	repo := PersonalAccountRepository{}
+	mockClock := new(MockClock)
+	repo := PersonalAccountRepository{Clock: mockClock}
+	date := time.Date(1996, time.October, 9, 0, 0, 0, 0, time.UTC)
+	mockClock.On("Now").Return(date)
 
 	repo.Deposit(1000)
 	transactions := repo.GetAll()
 	assert.Equal(t, len(transactions), 1)
 	assert.Equal(t, transactions[0].Amount, 1000)
+	assert.Equal(t, transactions[0].Date, date)
 }
 
 func TestPersonalAccountRepository_Withdraw(t *testing.T) {
-	repo := PersonalAccountRepository{}
+	mockClock := new(MockClock)
+	repo := PersonalAccountRepository{Clock: mockClock}
+	date := time.Date(1996, time.October, 10, 0, 0, 0, 0, time.UTC)
+	mockClock.On("Now").Return(date)
 
 	repo.Withdraw(1000)
 	transactions := repo.GetAll()
 	assert.Equal(t, len(transactions), 1)
 	assert.Equal(t, transactions[0].Amount, -1000)
+	assert.Equal(t, transactions[0].Date, date)
+}
+
+type MockClock struct {
+	mock.Mock
+}
+
+func (m *MockClock) Now() time.Time {
+	args := m.Called()
+	return args.Get(0).(time.Time)
 }
